57f7496f74a299438a744ce1cb7221e0b60e9850
Dovavivavi <szilagyiabel1@gmail.com>
2022-10-21 14:54:42 +0200

    basic structure

---
+++ script.js
@@ -0,0 +1,30 @@
+import movies from './data.js';
+console.log(movies);
+
+const baseHtml = function() {
+  return /*html*/ `
+    <div class="header">
+      <p>MEANINGFUL MOVIES</p>
+      <div>logo</div>
+    </div>
+    <div class="card-container"></div>
+  `
+};
+
+const root = document.querySelector('#root');
+root.insertAdjacentHTML('beforeend', baseHtml());
+
+const cardContainer = document.querySelector('.card-container');
+
+cardContainer.innerHTML = movies.cards.map(movie => /*html*/ `
+  <div class="card">
+    <div class="text-container">
+      <h1 class="title">${movie.title}</h1>
+      <p class="sub">${movie.sub}</p>
+      <p class="text">${movie.text}</p>
+    </div>
+    <div class="button-container">
+      <button>Watch</button>
+    </div>
+  </div>
+`).join('');
